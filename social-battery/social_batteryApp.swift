0b8e47b5820604a9352f9dc92428b50e388e7f38
Nicholas Morris <nic.morris@me.com>
2025-11-05 16:24:17 +1100

    Initial Commit

---
+++ social-battery/social_batteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  social_batteryApp.swift
+//  social-battery
+//
+//  Created by Nicholas Morris on 5/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct social_batteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ social-batteryTests/social_batteryTests.swift
@@ -0,0 +1,16 @@
+//
+//  social_batteryTests.swift
+//  social-batteryTests
+//
+//  Created by Nicholas Morris on 5/11/2025.
+//
+
+import Testing
+
+struct social_batteryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
